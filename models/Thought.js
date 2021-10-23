1361f1fef8e4b1de2c850fedfd2cda3448ef3215
brookemadison <brookeperry611@gmail.com>
2021-10-22 22:45:47 -0400

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+const dayjs = require('dayjs');
+
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dayjs(createdAtVal).format('MMMM D, YYYY h:mm A')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dayjs(createdAtVal).format('MMMM D, YYYY h:mm A')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
